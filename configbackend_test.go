8c08f972018e4548e4d7bc3425d707ce93d506ef
Ben Yanke <ben@benyanke.com>
2018-04-26 23:30:08 -0500

    test file added to begin work

---
+++ configbackend_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"testing"
+)
+
+
+// Functions to test somewhat easily
+//  * getGroupMembers
+//  * getGroupMemberIDs
+//  * getGroupDNs
+//  * getGroupName
+//  * Search - will have to probably look into mocking

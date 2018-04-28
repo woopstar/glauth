ec898a7cf31c2931d8c784701b7bc365b666d37a
Ben Yanke <ben@benyanke.com>
2018-04-28 15:54:35 -0500

    Adding notes about testing

--- configbackend_test.go
+++ configbackend_test.go
@@ -5,9 +5,17 @@ import (
 )
 
 
-// Functions to test somewhat easily
+// Functions to test easily
 //  * getGroupMembers
 //  * getGroupMemberIDs
 //  * getGroupDNs
 //  * getGroupName
+
+// Functions which are more difficult to test
+//  * Bind
+//  * Close - not even sure?
 //  * Search - will have to probably look into mocking
+
+
+// TODO before testing:
+//  Perhaps take a look at splitting up bind() and search()
